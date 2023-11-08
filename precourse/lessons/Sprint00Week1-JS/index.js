8c3369e870c8e226c30b5596e6992615b0c29694
sergeistetsko <sergeistetsko2021@yandex.by>
2023-11-08 14:44:47 +0300

    add variables, primitive data, var, let, const

--- precourse/lessons/Sprint00Week1-JS/index.js
+++ precourse/lessons/Sprint00Week1-JS/index.js
@@ -1,2 +1,14 @@
 console.log('hello, it-incubator');
 document.write('<h1>', 'Hello, world!', '</h1>');
+
+// data
+
+let name = 'it-incubator';
+let graduatesCount = 2000;
+let areYouChampion = true;
+
+// render
+
+document.write('<h1>', name, '</h1>');
+document.write('<input type="number" value="' + graduatesCount + '">');
+document.write('<input type="checkbox"', areYouChampion ? ' checked ': '', '>');
