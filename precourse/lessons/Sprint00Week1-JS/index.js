8c30a41e20de90fa448b2006d10b77e6f5b6667b
sergeistetsko <sergeistetsko2021@yandex.by>
2023-11-09 06:51:19 +0300

    add array of transaction objects

--- precourse/lessons/Sprint00Week1-JS/index.js
+++ precourse/lessons/Sprint00Week1-JS/index.js
@@ -1,38 +1,3 @@
-console.log('hello, it-incubator');
-document.write('<h1>', 'Hello, world!', '</h1>');
-
-// data
-
-let name = 'it-incubator';
-let graduatesCount = 2000;
-let areYouChampion = true;
-
-// render
-
-document.write('<h1>', name, '</h1>');
-document.write('<input type="number" value="' + graduatesCount + '">');
-document.write('<input type="checkbox"', areYouChampion ? ' checked ': '', '>');
-
-let student = {
-    info: {
-        firstName: 'Ivan',
-        lastName: 'Ivanov'
-    },
-    address: {
-        country: 'France',
-        city: 'Paris',
-    }
-}
-
-document.write(student.info.firstName);
-document.write(student.info.lastName);
-document.write(student.address.country);
-document.write(student.address.city);
-
-//data
-let currentUser = {
-    name: 'Sergei'
-}
 let card1 = {
     type: 'debit',
     networkType: 'mastercard',
@@ -88,53 +53,10 @@ let card2 = {
     ]
 };
 
-// //render
-// document.write('<h1>', 'Hello, ' + currentUser.name,'</h1>');
-// document.write('<h2>', 'Cards: ', '</h2>');
-//
-// //render card 1
-// document.write('<div>');
-// document.write('<b>', card1.type + ' card', '</b>', '</br>');
-// document.write('<b>', card1.networkType, '</b>', '</br>');
-// document.write('<b>', 'current balance', '</b>', '</br>');
-// document.write('<b>', card1.currentBalance, '</b>');
-// document.write('<span>', card1.currentBalance, '</span>');
-// document.write('<h4>', card1.number, '</h4>');
-// document.write('<span>', card1.expirationMonth, '/', card1.expirationYear, '</span>');
-// document.write('</div>');
-//
-// //render card 2
-// document.write('<div>');
-// document.write('<b>', card2.type + ' card', '</b>', '</br>');
-// document.write('<b>', card2.networkType, '</b>', '</br>');
-// document.write('<b>', 'current balance', '</b>', '</br>');
-// document.write('<b>', card2.currentBalance, '</b>');
-// document.write('<span>', card2.currentBalance, '</span>');
-// document.write('<h4>', card2.number, '</h4>');
-// document.write('<span>', card2.expirationMonth, '/', card2.expirationYear, '</span>');
-// document.write('</div>');
-//
-// // Просто пример:
-// let marks = [{
-//     title: 'JS',
-//     value: 10,
-// },
-//     {
-//         title: 'CS',
-//         value: 6,
-//     }
-// ];
-// // отобразим в консоли все заголовки с оценками
-// for (let i = 0; i < marks.length; i++) {
-//     let mark = marks[i];
-//     console.log("title: " + mark.title, ", mark: " + mark.value);
-// }
-
 let cards = [card1, card2];
 
 for (let i = 0; i < cards.length; i++) {
     let card = cards[i];
-    
     // render card
     document.write('<div>');
     document.write('<b>', card.type + ' card', '</b>', '</br>');
@@ -146,25 +68,12 @@ for (let i = 0; i < cards.length; i++) {
     document.write('<span>', card.expirationMonth, '/', card.expirationYear, '</span>');
 
     document.write("<h2>", "History Transaction", "</h2>");
+
     document.write("<ul>");
-    
+
     for (let j = 0; j < card.transactions.length; j++) {
         let transaction = card.transactions[j];
-        document.write(
-            "<li>",
-            transaction.title,
-            ", ",
-            transaction.date,
-            ",",
-            transaction.amount,
-            "</li>");
-        console.log("title: " + mark.title, ", mark: " + mark.value);
-        
-    document.write("</ul>");
-
-    document.write('</div>');
-    document.write('<hr>');
-
-
-    console.log("title: " + card.title, ", mark: " + card.value);
+        document.write("<li>", transaction.title, ", ", transaction.date, ",", transaction.amount, "</li>");
+        document.write("</ul>");
+    }
 }
