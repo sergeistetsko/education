58249defcfd2042a831b52aa714dbf1d856eff33
sergeistetsko <sergeistetsko2021@yandex.by>
2023-11-09 06:44:43 +0300

    add array of transaction objects

--- precourse/lessons/Sprint00Week1-JS/index.js
+++ precourse/lessons/Sprint00Week1-JS/index.js
@@ -46,7 +46,19 @@ let card1 = {
         title: 'Order Revenue',
         amount: 874,
         date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
-    }
+    },
+    transactions: [
+        {
+            title: 'Order Revenue',
+            amount: 874,
+            date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
+        },
+        {
+            title: 'Order Revenue',
+            amount: 874,
+            date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
+        }
+    ]
 };
 let card2 = {
     type: 'credit',
@@ -61,55 +73,68 @@ let card2 = {
         title: 'Order Revenue',
         amount: 874,
         date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
-    }
+    },
+    transactions: [
+        {
+            title: 'Order Revenue',
+            amount: 874,
+            date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
+        },
+        {
+            title: 'Order Revenue',
+            amount: 874,
+            date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
+        }
+    ]
 };
 
-//render
-document.write('<h1>', 'Hello, ' + currentUser.name,'</h1>');
-document.write('<h2>', 'Cards: ', '</h2>');
-
-//render card 1
-document.write('<div>');
-document.write('<b>', card1.type + ' card', '</b>', '</br>');
-document.write('<b>', card1.networkType, '</b>', '</br>');
-document.write('<b>', 'current balance', '</b>', '</br>');
-document.write('<b>', card1.currentBalance, '</b>');
-document.write('<span>', card1.currentBalance, '</span>');
-document.write('<h4>', card1.number, '</h4>');
-document.write('<span>', card1.expirationMonth, '/', card1.expirationYear, '</span>');
-document.write('</div>');
-
-//render card 2
-document.write('<div>');
-document.write('<b>', card2.type + ' card', '</b>', '</br>');
-document.write('<b>', card2.networkType, '</b>', '</br>');
-document.write('<b>', 'current balance', '</b>', '</br>');
-document.write('<b>', card2.currentBalance, '</b>');
-document.write('<span>', card2.currentBalance, '</span>');
-document.write('<h4>', card2.number, '</h4>');
-document.write('<span>', card2.expirationMonth, '/', card2.expirationYear, '</span>');
-document.write('</div>');
-
-// Просто пример:
-let marks = [{
-    title: 'JS',
-    value: 10,
-},
-    {
-        title: 'CS',
-        value: 6,
-    }
-];
-// отобразим в консоли все заголовки с оценками
-for (let i = 0; i < marks.length; i++) {
-    let mark = marks[i];
-    console.log("title: " + mark.title, ", mark: " + mark.value);
-}
+// //render
+// document.write('<h1>', 'Hello, ' + currentUser.name,'</h1>');
+// document.write('<h2>', 'Cards: ', '</h2>');
+//
+// //render card 1
+// document.write('<div>');
+// document.write('<b>', card1.type + ' card', '</b>', '</br>');
+// document.write('<b>', card1.networkType, '</b>', '</br>');
+// document.write('<b>', 'current balance', '</b>', '</br>');
+// document.write('<b>', card1.currentBalance, '</b>');
+// document.write('<span>', card1.currentBalance, '</span>');
+// document.write('<h4>', card1.number, '</h4>');
+// document.write('<span>', card1.expirationMonth, '/', card1.expirationYear, '</span>');
+// document.write('</div>');
+//
+// //render card 2
+// document.write('<div>');
+// document.write('<b>', card2.type + ' card', '</b>', '</br>');
+// document.write('<b>', card2.networkType, '</b>', '</br>');
+// document.write('<b>', 'current balance', '</b>', '</br>');
+// document.write('<b>', card2.currentBalance, '</b>');
+// document.write('<span>', card2.currentBalance, '</span>');
+// document.write('<h4>', card2.number, '</h4>');
+// document.write('<span>', card2.expirationMonth, '/', card2.expirationYear, '</span>');
+// document.write('</div>');
+//
+// // Просто пример:
+// let marks = [{
+//     title: 'JS',
+//     value: 10,
+// },
+//     {
+//         title: 'CS',
+//         value: 6,
+//     }
+// ];
+// // отобразим в консоли все заголовки с оценками
+// for (let i = 0; i < marks.length; i++) {
+//     let mark = marks[i];
+//     console.log("title: " + mark.title, ", mark: " + mark.value);
+// }
 
 let cards = [card1, card2];
 
 for (let i = 0; i < cards.length; i++) {
     let card = cards[i];
+    
     // render card
     document.write('<div>');
     document.write('<b>', card.type + ' card', '</b>', '</br>');
@@ -122,7 +147,19 @@ for (let i = 0; i < cards.length; i++) {
 
     document.write("<h2>", "History Transaction", "</h2>");
     document.write("<ul>");
-    document.write("<li>", card[i].transaction.title, ", ", card[i].transaction.date, ",", card[i].transaction.amount, "</li>");
+    
+    for (let j = 0; j < card.transactions.length; j++) {
+        let transaction = card.transactions[j];
+        document.write(
+            "<li>",
+            transaction.title,
+            ", ",
+            transaction.date,
+            ",",
+            transaction.amount,
+            "</li>");
+        console.log("title: " + mark.title, ", mark: " + mark.value);
+        
     document.write("</ul>");
 
     document.write('</div>');
