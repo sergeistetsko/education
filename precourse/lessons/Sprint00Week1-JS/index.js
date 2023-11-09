ae1645a57b6330b2eaae3f57a2c8a25a84706001
sergeistetsko <sergeistetsko2021@yandex.by>
2023-11-09 08:23:46 +0300

    add css

--- precourse/lessons/Sprint00Week1-JS/index.js
+++ precourse/lessons/Sprint00Week1-JS/index.js
@@ -50,8 +50,20 @@ let cards = [card1, card2];
 for (let i = 0; i < cards.length; i++) {
     let card = cards[i];
     // render card
+    document.write('<div class="card-block">');
     document.write('<b>', card.type + ' card', '</b>', '</br>');
-    document.write('<b>', card.networkType, '</b>', '</br>');
+    
+    let networkLogo = '';
+    switch (card.networkType) {
+        case 'visa':
+            networkLogo = '<img src="visa_logo.png">';
+            break;
+        case 'mastercard':
+            networkLogo = '<img src="mastercard_logo.png">';
+            break;
+            
+    }
+    document.write(networkLogo);
     document.write('<b>', 'current balance', '</b>', '</br>');
     
     let currencySign = "";
