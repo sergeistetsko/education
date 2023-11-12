a24f6f34b0580e1c246c77e06ef0523d0fede899
sergeistetsko <sergeistetsko2021@yandex.by>
2023-11-12 14:19:55 +0300

    add 4

--- precourse/homework/Sprint00Week1-JS/00/js.js
+++ precourse/homework/Sprint00Week1-JS/00/js.js
@@ -1,15 +1,28 @@
 // console.log('hello, it-incubator');
 // document.write('<h1>', 'hello, it-incubator', '</h1>');
+/*
+//data
 
 let name = 'it-incubator';
 let graduatesCount = 2000;
 let areYouChampion = true;
 
+//ui program, render
+
 document.write('<h1>', name, '</h1>');
-document.write('<input type="number" value="' + graduatesCount + '">');
-document.write('<input type="checkbox"', areYouChampion ? ' checked ': '', '>');
+document.write(`<input type="number" value="${graduatesCount}">`);
+document.write('<input type="checkbox"', areYouChampion ? ' checked ': '', '>');*/
 
-
+let student = {
+    info: {
+        firstName: 'Ivan',
+        lastName: 'Ivanov'
+    },
+    address: {
+        country: 'France',
+        city: 'Paris',
+    }
+}
 
 
 
