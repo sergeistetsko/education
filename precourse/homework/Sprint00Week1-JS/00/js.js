ea9c1d36b37cb2afcbc7aa8287bfd670ad4df743
sergeistetsko <sergeistetsko2021@yandex.by>
2023-11-12 14:50:01 +0300

    add 6

--- precourse/homework/Sprint00Week1-JS/00/js.js
+++ precourse/homework/Sprint00Week1-JS/00/js.js
@@ -13,7 +13,7 @@ document.write('<h1>', name, '</h1>');
 document.write(`<input type="number" value="${graduatesCount}">`);
 document.write('<input type="checkbox"', areYouChampion ? ' checked ': '', '>');*/
 
-let student = {
+/*let student = {
     info: {
         firstName: 'Ivan',
         lastName: 'Ivanov'
@@ -27,5 +27,14 @@ let student = {
 document.write(student.info.firstName);
 document.write(student.info.lastName);
 document.write(student.address.country);
-document.write(student.address.city);
+document.write(student.address.city);*/
 
+let card1 = {
+    type: 'debit',
+    networkType: 'visa',
+    currencyType: 1,
+    balance: 5750.53,
+    number: 5282345678901289,
+    expirationYear: 2025,
+    expirationMonth: 9
+};
