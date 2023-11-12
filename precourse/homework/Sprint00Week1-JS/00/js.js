10fb0429d2e8801486070086aed3f91ab64ee392
sergeistetsko <sergeistetsko2021@yandex.by>
2023-11-12 16:56:43 +0300

    add 11

--- precourse/homework/Sprint00Week1-JS/00/js.js
+++ precourse/homework/Sprint00Week1-JS/00/js.js
@@ -41,7 +41,12 @@ let card1 = {
     number: 5282345678901289,
     expirationYear: 2025,
     expirationMonth: 9,
-    isActive: true
+    isActive: true,
+    transaction: {
+        title: 'Order Revenue',
+        amount: 874,
+        date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
+    }
 };
 
 let card2 = {
@@ -52,10 +57,29 @@ let card2 = {
     number: 9232654321012032,
     expirationYear: 2026,
     expirationMonth: 4,
-    isActive: false
+    isActive: false,
+    transaction: {
+        title: 'Order Revenue',
+        amount: 874,
+        date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
+    }
 };
-
-let cards = [card1,card2];
+let card3 = {
+    type: 'debit',
+    networkType: 'mastercard',
+    currencyType: 'GBP',
+    currentBalance: 3263.65,
+    number: 554259349582930,
+    expirationYear: 2028,
+    expirationMonth: 3,
+    isActive: true,
+    transaction: {
+        title: 'Order Revenue',
+        amount: 874,
+        date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
+    }
+};
+let cards = [card1,card2,card3];
 
 //render
 
@@ -73,6 +97,16 @@ for (let i = 0; i < cards.length; i++) {
     document.write('<span>', card.currentBalance, '</span>', '</br>');
     document.write('<h4>', card.number, '</h4>', '</br>');
     document.write('<span>', card.expirationMonth, '/', card.expirationYear,  '</span>', '</br>');
+    document.write("<h2>", "History Transaction", "</h2>");
+    document.write("<ul>");
+    document.write("<li>",
+        card.transaction.title,
+        ", ",
+        card.transaction.date,
+        ",",
+        card.transaction.amount,
+        "</li>");
+    document.write("</ul>");
     document.write('</div>', '</br>');
 }
 /*
