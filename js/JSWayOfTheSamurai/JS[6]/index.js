411898000275ca80b9cf798a2de6497289d1535d
sergeistetsko <sergeistetsko2021@yandex.by>
2023-11-13 07:05:46 +0300

    add let, variable

--- js/JSWayOfTheSamurai/JS[6]/index.js
+++ js/JSWayOfTheSamurai/JS[6]/index.js
@@ -1,8 +1,16 @@
-alert('Wassup');
-alert('Здарова');
-alert('Bye');
-alert('Покеда');
-alert('Programmer');
-alert('Программист');
+let word0 = 'Wassup';
+let word1 = 'Bye';
+let word2 = 'Programmer';
+
+let wordTranslation0 = 'Здарова';
+let wordTranslation1 = 'Покеда';
+let wordTranslation2 = 'Программист';
+
+alert(word0);
+alert(wordTranslation0);
+alert(word1);
+alert(wordTranslation1);
+alert(word2);
+alert(wordTranslation2);
 
 alert('Финиш');
