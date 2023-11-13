c67115c040294c0bf279ee1045585c555fdf6546
sergeistetsko <sergeistetsko2021@yandex.by>
2023-11-13 10:14:49 +0300

    add step 6

--- js/JSWayOfTheSamurai/JS[9]/index.js
+++ js/JSWayOfTheSamurai/JS[9]/index.js
@@ -7,9 +7,13 @@ document.write('START', '<br>');
 const productPriceString1 = prompt('How much does product cost?');
 const productPrice1 = window.Number(productPriceString1);
 
-if (balance >= productPriceString1) {
-    document.write('Product purchased');
+if (isNaN(productPrice1)) {
+    if (balance >= productPriceString1) {
+        document.write('Product purchased');
+    } else {
+        document.write('Not enough money');
+    }
 } else {
-    document.write('Not enough money');
+    document.write('You input incorrect number');
 }
 document.write('<br>','END');
