efaa53e59042d1232643f245b944cd274c2df5e3
sergeistetsko <sergeistetsko2021@yandex.by>
2023-11-13 14:02:18 +0300

    add step 15

--- js/JSWayOfTheSamurai/JS[12]/index.js
+++ js/JSWayOfTheSamurai/JS[12]/index.js
@@ -45,6 +45,7 @@ let person = persons[1].birthYear;
 
 console.log(person);*/
 
+/*
 let shoppingLists = [
     ['молоко', 'xлеб', 'яйца'],
     ['сок', 'мороженое'],
@@ -53,4 +54,60 @@ let shoppingLists = [
 console.log(shoppingLists[1][0]);
 
 let shoppingListsLength = shoppingLists.length;
-console.log(shoppingListsLength);
+console.log(shoppingListsLength);*/
+
+const words = [
+    {
+        original: 'Wassup',
+        translation: 'Здарова'
+    },
+    {
+        original: 'Bye',
+        translation: 'Покеда'
+    },
+    {
+        original: 'Programmer',
+        translation: 'Программист'
+    }
+]
+
+const notificationMessages = {
+    start: {
+        hello: 'Hello man. You can learn English. Good Luck'
+    },
+    result: {
+        finishSucces: 'Молодец. Good result!',
+        finishUnsucces: 'Молодец. Но постарайся лучше!'
+    }
+}
+const settings = {
+    correctAnswersMinPercent: 50
+}
+const result = {
+    correctAnswersCount: 0
+}
+
+alert(notificationMessages.start.hello);
+
+const userAnswer0 = prompt(words[0].original);
+alert(userAnswer0 === words[0].translation);
+if (userAnswer0 === words[0].translation) {
+    result.correctAnswersCount = result.correctAnswersCount + 1;
+}
+const userAnswer1 = prompt(words[1].original);
+alert(userAnswer1 === words[1].translation);
+if (userAnswer1 === words[1].translation) {
+    result.correctAnswersCount = result.correctAnswersCount + 1;
+}
+const userAnswer2 = prompt(words[2].original);
+alert(userAnswer2 === words[2].translation);
+if (userAnswer2 === words[2].translation) {
+    result.correctAnswersCount = result.correctAnswersCount + 1;
+}
+const userCorrectAnswersPercent = result.correctAnswersCount / words.length * 100;
+
+if (userCorrectAnswersPercent > settings.correctAnswersMinPercent) {
+    alert(notificationMessages.result.finishSucces);
+} else {
+    alert(notificationMessages.result.finishUnsucces);
+}
