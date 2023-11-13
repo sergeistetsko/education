04d43ba6d2ab172fdfe2a7a25cf2d80cc737a857
sergeistetsko <sergeistetsko2021@yandex.by>
2023-11-13 13:25:13 +0300

    add step 7

--- js/JSWayOfTheSamurai/JS[12]/index.js
+++ js/JSWayOfTheSamurai/JS[12]/index.js
@@ -7,7 +7,7 @@ let grade5 = 9;
 console.log(grade1);
 console.log(grade4);*/
 
-let grades = [5,6,7,8,9];
+/*let grades = [5,6,7,8,9];
 
 let extractedGrade = grades[4];
 
@@ -15,4 +15,10 @@ console.log(grades[0]);
 console.log(grades[3]);
 console.log(grades[10]);
 
-console.log(extractedGrade)
+console.log(extractedGrade)*/
+
+let books = ['Война и мир', 'Мастер и Маргарита', 'Преступление и наказание'];
+let masterAndMargoBook = books[1];
+console.log(books[1]);
+
+
