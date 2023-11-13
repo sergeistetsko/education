c1d87a029b7b9c18f1e2b43546a96acd8bb0cb00
sergeistetsko <sergeistetsko2021@yandex.by>
2023-11-13 13:12:40 +0300

    add step 4

--- js/JSWayOfTheSamurai/JS[12]/index.js
+++ js/JSWayOfTheSamurai/JS[12]/index.js
@@ -1,9 +1,10 @@
-let grade1 = 5;
+/*let grade1 = 5;
 let grade2 = 6;
 let grade3 = 7;
 let grade4 = 8;
 let grade5 = 9;
 
 console.log(grade1);
-console.log(grade4);
+console.log(grade4);*/
 
+let grades = [5,6,7,8,9];
