206e3f0ab8703616d8230c4d71d62a2ba41b1970
sergeistetsko <sergeistetsko2021@yandex.by>
2023-11-13 11:29:42 +0300

    add step 4

--- js/JSWayOfTheSamurai/JS[11]/index.js
+++ js/JSWayOfTheSamurai/JS[11]/index.js
@@ -5,3 +5,19 @@ let myConsole = {
 window.alert(myConsole.age);
 myConsole.age = 31;
 window.alert(myConsole.age);
+
+const laptop1 = {
+    name: 'zenbook',
+    manufacturer: 'ASUS',
+    price: 1000
+}
+const laptop2 = {
+    name: 'macbook',
+    manufacturer: 'APPLE',
+    price: 3000
+}
+const moneyTransfering = {
+    from: 'Sergey',
+    to: 'Dima',
+    amount: 10
+}
