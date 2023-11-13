da7ddc609074ab149fe4ad0889950d430bb86a16
sergeistetsko <sergeistetsko2021@yandex.by>
2023-11-13 14:18:34 +0300

    add step 2

---
+++ js/JSWayOfTheSamurai/JS[13]/index.js
@@ -0,0 +1,32 @@
+const repeatCount = 1;
+
+let index = 1;
+if (index <= repeatCount) {
+    document.write('a');
+}
+index = 2;
+if (index <= repeatCount) {
+    document.write('a');
+}
+index = 3;
+if (index <= repeatCount) {
+    document.write('a');
+}
+index = 4;
+if (index <= repeatCount) {
+    document.write('a');
+}
+index = 5;
+if (index <= repeatCount) {
+    document.write('a');
+}
+index = 6;
+if (index <= repeatCount) {
+    document.write('a');
+}
+index = 7;
+if (index <= repeatCount) {
+    document.write('a');
+}
+
+
