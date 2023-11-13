f80e1e34554dfd8a31ee198db143a14845298ca5
sergeistetsko <sergeistetsko2021@yandex.by>
2023-11-13 09:45:31 +0300

    add step 3

--- js/JSWayOfTheSamurai/JS[9]/index.js
+++ js/JSWayOfTheSamurai/JS[9]/index.js
@@ -1,6 +1,11 @@
+let balance = 100;
+const productPrice1 = 110;
+
+// ------------
+
 document.write('START', '<br>');
 
-if (true) {
+if (balance >= productPrice1) {
     document.write('Product purchased');
 } else {
     document.write('Not enough money');
