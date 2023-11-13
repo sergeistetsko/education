988598e131a573cc1baedcd5b0c4b3983e6399f6
sergeistetsko <sergeistetsko2021@yandex.by>
2023-11-13 09:12:33 +0300

    add step 2

---
+++ js/JSWayOfTheSamurai/JS[8]/index.js
@@ -0,0 +1,13 @@
+/*
+window.alert('it-incubator');
+document.alert('it-incubator');
+console.log('it-incubator');
+*/
+
+const userInput = window.prompt('write something');
+document.write("user input is: " + userInput);
+document.write('<br>');
+document.write("user input is: " + userInput);
+document.write('<br>');
+document.write("user input is: " + userInput);
+
