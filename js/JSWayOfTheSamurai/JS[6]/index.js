5e90c30988094d11bc954969e2c6159e9573d379
sergeistetsko <sergeistetsko2021@yandex.by>
2023-11-13 07:25:41 +0300

    add step 4

--- js/JSWayOfTheSamurai/JS[6]/index.js
+++ js/JSWayOfTheSamurai/JS[6]/index.js
@@ -17,12 +17,21 @@ let correctAnswersCount = 0;
 // ---------------------------
 
 const userAnswer0 = prompt(word0);
-alert(wordTranslation0);
+alert(wordTranslation0 === wordTranslation0);
+if (wordTranslation0 === wordTranslation0) {
+    correctAnswersCount = correctAnswersCount + 1;
+}
 
 const userAnswer1 = prompt(word1);
-alert(wordTranslation1);
+alert(wordTranslation1 === wordTranslation1);
+if (wordTranslation1 === wordTranslation1) {
+    correctAnswersCount = correctAnswersCount + 1;
+}
 
 const userAnswer2 = prompt(word2)
-alert(wordTranslation2);
+alert(wordTranslation2 === wordTranslation2);
+if (wordTranslation2 === wordTranslation2) {
+    correctAnswersCount = correctAnswersCount + 1;
+}
 
 alert(finishMessage);
