133c2c713abb2c213fafcc53667aab0f2c6c691d
sergeistetsko <sergeistetsko2021@yandex.by>
2023-12-02 14:33:37 +0300

    add playlist

--- precourse/lessons/Sprint00Week4JS/src/data.js
+++ precourse/lessons/Sprint00Week4JS/src/data.js
@@ -1,120 +1,75 @@
-export function getCurrentUser() {
-    let newUser = {
-        name: "Alex",
-        role: "Admin",
-        balance: 100,
-    };
-    return newUser;
+let songs = [
+    {
+        year: 2020,
+        artist: "Elvira T",
+        title: "Море",
+        url: '../Elvira T - Море.mp3'
+    },
+    {
+        year: 2019,
+        artist: "Lewis Capaldi",
+        title: "Someone You Loved"
+    },
+    {
+        year: 2018,
+        artist: "Drake",
+        title: "God's Plan"
+    },
+    {
+        year: 2017,
+        artist: "Ed Sheeran",
+        title: "Shape of You"
+    },
+    {
+        year: 2016,
+        artist: "Beyoncé",
+        title: "Formation"
+    }
+];
+
+let searchTerm = '';
+
+let subscriber = null;
+let sortDirection = null;
+
+// publisher-subscriber
+export function subscribe(subscriberCallback) {
+    subscriber = subscriberCallback;
 }
 
-let card1 = {
-    id: 1,
-    type: "debit",
-    networkType: "mastercard",
-    currencyType: "USD",
-    currentBalance: 5750.53,
-    number: 5282345678901289,
-    expirationYear: 2025,
-    expirationMonth: 9,
-    isActive: true,
-    transactions: [
-        {
-            title: "Order Revenue",
-            amount: 874,
-            date: new Date(2022, 3, 25, 16, 43, 22), // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
-        },
-        {
-            title: "Order Revenue",
-            amount: -874,
-            date: new Date(2022, 3, 25, 16, 43, 22), // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
-        },
-    ],
-};
+// getter
+export function getSongs() {
+    const filteredSongs = songs.filter(s => s.title.toLowerCase().indexOf(searchTerm) > -1 
+    || s.artist.toLowerCase().indexOf(searchTerm) > -1);
 
-let card2 = {
-    id: 2,
-    type: "credit",
-    networkType: "visa",
-    currencyType: "EUR",
-    currentBalance: 1343.53,
-    number: 9232654321012032,
-    expirationYear: 2026,
-    expirationMonth: 4,
-    isActive: false,
-    transactions: [
-        {
-            title: "Order Revenue",
-            amount: -874,
-            date: new Date(2022, 3, 25, 16, 43, 22), // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
-        },
-        {
-            title: "Order Revenue",
-            amount: -874,
-            date: new Date(2022, 3, 25, 16, 43, 22), // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
-        },
-    ],
-};
-
-let card3 = {
-    id: 3,
-    type: "debit",
-    networkType: "mastercard",
-    currencyType: "GBP",
-    currentBalance: 3263.65,
-    number: 554259349582930,
-    expirationYear: 2028,
-    expirationMonth: 3,
-    isActive: true,
-    //.. all existing properties
-    transactions: [
-        {
-            title: "Order Revenue",
-            amount: 874,
-            date: new Date(2022, 3, 25, 16, 43, 22), // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
-        },
-        {
-            title: "Order Revenue",
-            amount: 874,
-            date: new Date(2022, 3, 25, 16, 43, 22), // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
-        },
-    ],
-};
-
-let cards = [card1, card2, card3];
-
-let dataChangedEventListener = function () {
+    return filteredSongs;
 }
 
-
-export function addEventListener(listener) {
-    dataChangedEventListener = listener;
+// setter
+export function setSearchTerm(newSearchTerm) {
+    searchTerm = newSearchTerm.toLowerCase().trim();
+    subscriber();
 }
 
-
-export function getCards() {
-    return cards;
+// getter
+export function getSearchTerm() {
+    return searchTerm;
 }
+// getter/setter - инкапсуляция
 
-export function createCard(cardNumber, newCardType) {
-    let newCard = {
-        id: Number(new Date()),
-        type: "debit",
-        networkType: newCardType,
-        currencyType: "USD",
-        currentBalance: 0,
-        number: cardNumber,
-        expirationYear: 0,
-        expirationMonth: 0,
-        isActive: false,
-        transactions: [],
-    };
-    cards.unshift(newCard);
-    dataChangedEventListener();
-}
+/**
+ * 
+ * @param {'asc'|'desc'} direction 
+ */
+export function setSortDirection(direction) {
+    sortDirection = direction;
+    songs.sort((a,b) => {
+        if (direction === 'asc') return a.year - b.year;
+        return b.year - a.year;
+    });
+    subscriber();
+} 
 
-export function updateCard(id, newCardNumber, newCardType) {
-    const card = cards.find(card => card.id === id);
-    card.number = newCardNumber;
-    card.networkType = newCardType;
-    dataChangedEventListener();
+export function gerSortDirection() {
+    return sortDirection;
 }
--- precourse/lessons/Sprint00Week4JS/src/index.js
+++ precourse/lessons/Sprint00Week4JS/src/index.js
@@ -1,28 +1,19 @@
-import {getCurrentUser, getCards, addEventListener} from './data.js'
-import {renderHeader} from './components/header.js'
-import {Cards} from './components/cards/card.js'
+import { subscribe } from "./data.js";
+import { renderPlaylist } from "./components/playlist.js";
+import { renderFilterPanel } from "./components/filter-panel/filter-panel.js";
 
+const rootElement = document.querySelector("#root");
 
-// addEventListener(renderApp);
-addEventListener(renderApp);
+function renderApp() {
+  rootElement.innerHTML = "";
+ 
+  const filterPanelElement = renderFilterPanel();
+  rootElement.appendChild(filterPanelElement);
 
-function logData() {
-    console.log('DATA CHANGED');
-    console.log(getCards());
-}
-
-export function renderApp() {
-    const currentUser = getCurrentUser();
-    const cards = getCards();
-
-    const rootElement = document.querySelector('#root');
-    rootElement.innerHTML = '';
-
-    const headerElement = renderHeader(currentUser.name, currentUser.balance);
-    rootElement.appendChild(headerElement);
-
-    const cardsElement = Cards(cards, renderApp);
-    rootElement.appendChild(cardsElement);
+  const songsElement = renderPlaylist();
+  rootElement.appendChild(songsElement);
 }
 
 renderApp();
+
+subscribe(renderApp);
