0230ac072c52a5a6ddbf6b80662a60566d215fc8
sergeistetsko <sergeistetsko2021@yandex.by>
2023-12-06 16:16:20 +0300

    add fix

--- js/JSWayOfTheSamurai/JS[6]/index.js
+++ js/JSWayOfTheSamurai/JS[6]/index.js
@@ -1,48 +1,48 @@
 // Блок данных
 
-const word0 = 'Wassup';
-const word1 = 'Bye';
-const word2 = 'Programmer';
+const word0 = 'Wassup'
+const word1 = 'Bye'
+const word2 = 'Programmer'
 
-const wordTranslation0 = 'Здарова';
-const wordTranslation1 = 'Покеда';
-const wordTranslation2 = 'Программист';
+const wordTranslation0 = 'Здарова'
+const wordTranslation1 = 'Покеда'
+const wordTranslation2 = 'Программист'
 
-const wordsCount = 3;
+const wordsCount = 3
 
-const finishSuccessMessage = "Молодец. Good result!";
-const finishUnsuccessMessage = "Молодец. Но постарайся лучше!";
+const finishSuccessMessage = 'Молодец. Good result!'
+const finishUnsuccessMessage = 'Молодец. Но постарайся лучше!'
 
-const correctAnswersMinPercent = 50;
+const correctAnswersMinPercent = 50
 
-let correctAnswersCount = 0;
+let correctAnswersCount = 0
 
 // Блок логики
 
-const userAnswer0 = prompt(word0);
-alert(userAnswer0 === wordTranslation0);
-if (wordTranslation0 === wordTranslation0) {
-    correctAnswersCount = correctAnswersCount + 1;
+const userAnswer0 = prompt(word0)
+alert(userAnswer0 === wordTranslation0)
+if (userAnswer0 === wordTranslation0) {
+    correctAnswersCount = correctAnswersCount + 1
 }
 
-const userAnswer1 = prompt(word1);
-alert(userAnswer1 === wordTranslation1);
-if (wordTranslation1 === wordTranslation1) {
+const userAnswer1 = prompt(word1)
+alert(userAnswer1 === wordTranslation1)
+if (userAnswer1 === wordTranslation1) {
     correctAnswersCount = correctAnswersCount + 1;
 }
 
 const userAnswer2 = prompt(word2)
-alert(userAnswer2 === wordTranslation2);
-if (wordTranslation2 === wordTranslation2) {
-    correctAnswersCount = correctAnswersCount + 1;
+alert(userAnswer2 === wordTranslation2)
+if (userAnswer2 === wordTranslation2) {
+    correctAnswersCount = correctAnswersCount + 1
 }
 
-const userCorrectAnswersPercent = correctAnswersCount / wordsCount * 100;
+const userCorrectAnswersPercent = correctAnswersCount / wordsCount * 100
 
 if (userCorrectAnswersPercent > correctAnswersMinPercent) {
-    alert(finishSuccessMessage);
+    alert(finishSuccessMessage)
 } else {
-    alert(finishUnsuccessMessage);
+    alert(finishUnsuccessMessage)
 }
 
 
