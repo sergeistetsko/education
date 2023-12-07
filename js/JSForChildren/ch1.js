df3bdd59c1e063dc77d9be7df65a7af7f4d8454c
sergeistetsko <sergeistetsko2021@yandex.by>
2023-12-07 17:32:23 +0300

    add ch1 + ch2

---
+++ js/JSForChildren/ch1.js
@@ -0,0 +1,7 @@
+// Рисуем столько котиков, сколько захотим!
+let drawCats = function (howManyTimes) {
+    for (let i = 0; i < howManyTimes; i++) {
+        console.log (i + '=^.^=')
+    }
+}
+drawCats(10);
