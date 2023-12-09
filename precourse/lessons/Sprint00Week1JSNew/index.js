09b8d71c1e15cefaedf5d995122b00d065bfe800
sergeistetsko <sergeistetsko2021@yandex.by>
2023-12-09 22:08:51 +0300

    add part 3

--- precourse/lessons/Sprint00Week1JSNew/index.js
+++ precourse/lessons/Sprint00Week1JSNew/index.js
@@ -4,23 +4,42 @@ h1Element.append('Hello from IT-incubator')
 document.body.append(h1Element)
 
 // part 2
-
 // data
-const title = 'it-incubator'
-const graduatesCount = '2000'
-const areYouChampion = true
+/*
+let title = 'it-incubator'
+let graduatesCount = 2000
+let areYouChampion = true
+*/
+const incubatorInfo = {
+    title: 'it-incubator',
+    graduatesCount: 2000,
+    areYouChampion: true,
+    technologies: ['Front', 'Back', 'DevOps']    
+}
 
 // render
 let pageTitleElement = document.createElement('h1')
-pageTitleElement.innerText = title
+pageTitleElement.innerText = incubatorInfo.title
 document.body.append(pageTitleElement)
 
 let graduatesCountElement = document.createElement('input')
-graduatesCountElement.value = graduatesCount
+graduatesCountElement.value = incubatorInfo.graduatesCount
 graduatesCountElement.type = 'number'
 document.body.append(graduatesCountElement)
 
 let areYouChampionElement = document.createElement('input')
 areYouChampionElement.type = 'checkbox'
-areYouChampionElement.checked = areYouChampion
+areYouChampionElement.checked = incubatorInfo.areYouChampion
 document.body.append(areYouChampionElement)
+
+// part 3
+let techSelectElement = document.createElement('select')
+let techSelectOptions0Element = document.createElement('option')
+techSelectOptions0Element.append(incubatorInfo.technologies[0])
+let techSelectOptions1Element = document.createElement('option')
+techSelectOptions1Element.append(incubatorInfo.technologies[1])
+let techSelectOptions2Element = document.createElement('option')
+techSelectOptions2Element.append(incubatorInfo.technologies[2])
+
+techSelectElement.append(techSelectOptions0Element, techSelectOptions1Element, techSelectOptions2Element)
+document.body.append(techSelectElement)
