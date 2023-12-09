1317a4d6b6c2a67b69f284358e00d96d93bdffa4
sergeistetsko <sergeistetsko2021@yandex.by>
2023-12-09 21:25:22 +0300

    add part 1, 2 fix

--- precourse/lessons/Sprint00Week1JSNew/index.js
+++ precourse/lessons/Sprint00Week1JSNew/index.js
@@ -6,19 +6,21 @@ document.body.append(h1Element)
 // part 2
 
 // data
-const title = 'it-incubator';
-const graduatesCount = 2000;
-const areYouChampion = true;
+const title = 'it-incubator'
+const graduatesCount = '2000'
+const areYouChampion = true
+
 // render
-let pageTitleElement = document.createElement('h1');
-pageTitleElement.innerText = title;
-document.body.append(pageTitleElement);
+let pageTitleElement = document.createElement('h1')
+pageTitleElement.innerText = title
+document.body.append(pageTitleElement)
 
-let graduatesCountElement = document.createElement('input');
-graduatesCountElement.value = graduatesCount;
-document.body.append(graduatesCountElement);
+let graduatesCountElement = document.createElement('input')
+graduatesCountElement.value = graduatesCount
+graduatesCountElement.type = 'number'
+document.body.append(graduatesCountElement)
 
-let areYouChampionElement = document.createElement('input');
-areYouChampionElement.type = 'checkbox';
-areYouChampionElement.checked = areYouChampion;
-document.body.append(areYouChampionElement);
+let areYouChampionElement = document.createElement('input')
+areYouChampionElement.type = 'checkbox'
+areYouChampionElement.checked = areYouChampion
+document.body.append(areYouChampionElement)
