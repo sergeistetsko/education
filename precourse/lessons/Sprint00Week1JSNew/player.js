0139bc030ce726ec740d4cd81f7792b3a03726b6
sergeistetsko <sergeistetsko2021@yandex.by>
2023-12-11 15:44:18 +0300

    add render

--- precourse/lessons/Sprint00Week1JSNew/player.js
+++ precourse/lessons/Sprint00Week1JSNew/player.js
@@ -24,3 +24,28 @@ let playlist = {
     ]
 }
 // render
+    // render playlist header
+        // render image
+let playlistImageElement = document.createElement('img')
+playlistImageElement.src = playlist.coverImageUrl
+document.body.append(playlistImageElement)
+
+        // render title
+let playlistTitleElement = document.createElement('h2')
+playlistTitleElement.append(playlist.title)
+document.body.append(playlistTitleElement)
+
+let tracksListElement = document.createElement('ul')
+
+        // render track1
+let track1Element = document.createElement('li')
+track1Element.append(playlist.tracks[0].artistName + ' - ' + playlist.tracks[0].title)
+tracksListElement.append(track1Element)
+
+        // render track2
+let track2Element = document.createElement('li')
+track2Element.append(playlist.tracks[1].artistName + ' - ' + playlist.tracks[1].title)
+tracksListElement.append(track2Element)
+
+document.body.append(tracksListElement)
+
