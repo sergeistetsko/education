1e12411f226d710b12619e507b877a6fe599b71b
sergeistetsko <sergeistetsko2021@yandex.by>
2023-12-11 15:21:35 +0300

    add data

---
+++ precourse/lessons/Sprint00Week1JSNew/player.js
@@ -0,0 +1,24 @@
+// data
+let playlist = {
+    title: 'Hip-Hop Hits',
+    coverImageUrl: './playlist1.png',
+    info: {
+        totalTracksCount: 4,
+        totalTracksDurationInSeconds: 733
+    },
+    tracks: [
+        {
+            artistName: 'Eminem',
+            title: 'Rap God',
+            fileUrl: './Eminem_-_Rap_God.mp3',
+            isHot: false
+        },
+        {
+            artistName: '50 Cent',
+            title: 'In da Club',
+            fileUrl: './50_Cent_-_In_da_Club.mp3',
+            isHot: true
+        }
+    ]
+}
+// render
