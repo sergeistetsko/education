0965a9cf7eda9bc0f16cab2d6da900c29ed1db8f
sergeistetsko <sergeistetsko2021@yandex.by>
2023-12-12 10:07:48 +0300

    add task7

---
+++ precourse/lessons/Sprint00Week1JSOld/index7.js
@@ -0,0 +1,12 @@
+const color = 'black'
+
+switch (color) {
+    case 'red':
+        console.log('красный!')
+        break
+    case 'black':
+        console.log('чёрный!')
+        break
+    default:
+        console.log('другой цвет!')
+}
