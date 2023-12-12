bb413425376205a9e0fb8affe33c8cab9c8ae6d0
sergeistetsko <sergeistetsko2021@yandex.by>
2023-12-12 09:37:06 +0300

    add task5

---
+++ precourse/lessons/Sprint00Week1JSOld/index5.js
@@ -0,0 +1,16 @@
+let str1 = 'iop'
+let str2 = 'hjk'
+
+console.log((str1[1] + str2[2]).toUpperCase() + '!')
+
+console.log(1+2)
+console.log(5%2)
+console.log(5/2)
+console.log(5**2)
+console.log(5*2)
+
+let count = 2
+
+console.log(count + 1)
+console.log(count++)
+console.log(++count)
