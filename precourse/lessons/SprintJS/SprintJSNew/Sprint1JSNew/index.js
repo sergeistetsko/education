0f67574ab591671f71556ac086730b6edf512c07
sergeistetsko <sergeistetsko2021@yandex.by>
2023-12-20 05:39:02 +0300

    add comments

--- precourse/lessons/SprintJS/SprintJSNew/Sprint1JSNew/index.js
+++ precourse/lessons/SprintJS/SprintJSNew/Sprint1JSNew/index.js
@@ -1,15 +1,10 @@
-// part 1
+// Создание элемента и добавление его в DOM (апенд в документ)
 const h1Element = document.createElement('h1')
+
 h1Element.append('Hello from IT-incubator')
 document.body.append(h1Element)
 
-// part 2
-// data
-/*
-let title = 'it-incubator'
-let graduatesCount = 2000
-let areYouChampion = true
-*/
+// Создание объекта
 const incubatorInfo = {
     title: 'it-incubator',
     graduatesCount: 2000,
@@ -21,27 +16,39 @@ const incubatorInfo = {
     ]
 }
 
-// render
+// Создание элементов и добавление их в DOM
+// Создание элемента h1 и добавление его в body
 let pageTitleElement = document.createElement('h1')
+
 pageTitleElement.innerText = incubatorInfo.title
+
 document.body.append(pageTitleElement)
 
+// Создание элемента input, добавление ему атрибутов и добавление его в body
 let graduatesCountElement = document.createElement('input')
+
 graduatesCountElement.value = incubatorInfo.graduatesCount
 graduatesCountElement.type = 'number'
+
 document.body.append(graduatesCountElement)
 
+// Создание элемента input, добавление ему атрибутов и добавление его в body
 let areYouChampionElement = document.createElement('input')
+
 areYouChampionElement.type = 'checkbox'
 areYouChampionElement.checked = incubatorInfo.areYouChampion
+
 document.body.append(areYouChampionElement)
 
-// part 3
+// Создание элемента select, его options и добавление его в body
 let techSelectElement = document.createElement('select')
+
 let techSelectOptions0Element = document.createElement('option')
 techSelectOptions0Element.append(incubatorInfo.technologies[0].title)
+
 let techSelectOptions1Element = document.createElement('option')
 techSelectOptions1Element.append(incubatorInfo.technologies[1].title)
+
 let techSelectOptions2Element = document.createElement('option')
 techSelectOptions2Element.append(incubatorInfo.technologies[2].title)
 
