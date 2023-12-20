8c7d9a49707a2d39806875069472f20348410604
sergeistetsko <sergeistetsko2021@yandex.by>
2023-12-20 07:39:57 +0300

    add examples

--- precourse/lessons/SprintJS/SprintJSNew/Sprint2JSNew/index.js
+++ precourse/lessons/SprintJS/SprintJSNew/Sprint2JSNew/index.js
@@ -1,1 +1,18 @@
 // all done in playerV4
+//example 1
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+//example 2
+let numbers = [1, 2, 3, 4, 5]; // Пример массива чисел
+for (let i = 0; i < numbers.length; i++) {
+    const number = numbers[i]
+    console.log(number);
+}
+//example 3
+let objects = [{name: 'Alice'}, {name: 'Bob'}, {name: 'Charlie'}]; // Пример массива объектов
+for (let i = 0; i < objects.length; i++) {
+    const object = objects[i]
+    console.log(object.name);
+}
