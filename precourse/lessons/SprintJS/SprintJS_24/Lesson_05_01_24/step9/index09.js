b6586e5d8530005f7f75094847aab172ce79b13b
sergeistetsko <sergeistetsko2021@yandex.by>
2024-01-13 17:13:36 +0300

    step 9

---
+++ precourse/lessons/SprintJS/SprintJS_24/Lesson_05_01_24/step9/index09.js
@@ -0,0 +1,44 @@
+// data
+const info = {
+    title: 'it-incubator',
+    graduatesCount: 2000,
+    areYouChampion: true,
+    technologies: ['Front', 'Back', 'DevOps']
+}
+
+// render
+let pageTitleElement = document.createElement('h1');
+
+pageTitleElement.innerText = info.title;
+
+document.body.append(pageTitleElement);
+
+let graduatesCountElement = document.createElement('input');
+
+graduatesCountElement.value = info.graduatesCount;
+
+document.body.append(graduatesCountElement);
+
+let areYouChampionElement = document.createElement('input');
+
+areYouChampionElement.type = 'checkbox';
+areYouChampionElement.checked = info.areYouChampion;
+
+document.body.append(areYouChampionElement);
+
+let techSelectElement = document.createElement('select');
+
+let techSelectOption0Element= document.createElement('option');
+techSelectOption0Element.append(info.technologies[0]);
+techSelectElement.append(techSelectOption0Element);
+
+let techSelectOption1Element= document.createElement('option');
+techSelectOption1Element.append(info.technologies[1]);
+techSelectElement.append(techSelectOption1Element);
+
+let techSelectOption2Element= document.createElement('option');
+techSelectOption2Element.append(info.technologies[2]);
+techSelectElement.append(techSelectOption2Element);
+
+document.body.append(techSelectElement);
+
