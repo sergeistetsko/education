4d537237d2266f51673fadff3e600704f866fdf5
sergeistetsko <sergeistetsko2021@yandex.by>
2024-01-13 16:56:21 +0300

    step 7 and 8

---
+++ precourse/lessons/SprintJS/SprintJS_24/Lesson_05_01_24/step8/index08.js
@@ -0,0 +1,27 @@
+// data
+const info = {
+    title: 'it-incubator',
+    graduatesCount: 2000,
+    areYouChampion: true
+}
+
+// render
+let pageTitleElement = document.createElement('h1');
+
+pageTitleElement.innerText = info.title;
+
+document.body.append(pageTitleElement);
+
+let graduatesCountElement = document.createElement('input');
+
+graduatesCountElement.value = info.graduatesCount;
+
+document.body.append(graduatesCountElement);
+
+let areYouChampionElement = document.createElement('input');
+
+areYouChampionElement.type = 'checkbox';
+areYouChampionElement.checked = info.areYouChampion;
+
+document.body.append(areYouChampionElement);
+
