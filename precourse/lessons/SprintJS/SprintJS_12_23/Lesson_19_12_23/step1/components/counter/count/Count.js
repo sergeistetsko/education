e5789b90bfa768339db8cdd91a7ec53a8d7d74ea
sergeistetsko <sergeistetsko2021@yandex.by>
2024-01-17 11:50:14 +0300

    add step 1 counter

---
+++ precourse/lessons/SprintJS/SprintJS_12_23/Lesson_19_12_23/step1/components/counter/count/Count.js
@@ -0,0 +1,5 @@
+export function Count(clientsCount) {
+    const element = document.createElement('div')
+    element.append(clientsCount)
+    return element
+}
---
+++ precourse/lessons/SprintJS/SprintJS_12_23/Lesson_19_12_23/step1/components/counter/renderCounter.js
@@ -0,0 +1,8 @@
+import {Header} from './header/Header.js'
+import {Count} from './count/Count.js'
+import {Button} from './button/Button.js'
+export function renderCounter(data) {
+    document.body.append(Header())
+    document.body.append(Count(data.clientsCount))
+    document.body.append(Button())
+}
