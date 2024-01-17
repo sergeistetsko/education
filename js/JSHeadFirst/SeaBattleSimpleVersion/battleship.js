af3465da34d0aefda6f976436e7a1904e5fafbb1
sergeistetsko <sergeistetsko2021@yandex.by>
2024-01-17 16:11:36 +0300

    add Battle Ship Simple Version

---
+++ js/JSHeadFirst/SeaBattleSimpleVersion/battleship.js
@@ -0,0 +1,34 @@
+// data
+const location1 = 3
+const location2 = 4
+const location3 = 5
+
+let guess
+let hits = 0
+let guesses = 0
+
+let isSunk = false
+
+// logics
+
+while (isSunk == false) {
+    guess = prompt('Ready, aim, fire! (enter a number 0-6):')
+    if (guess < 0 || guess > 6) {
+        alert('Please enter a valid cell number!')
+    } else {
+        guesses = guesses + 1
+        if (guess == location1 || guess == location2 || guess == location3) {
+            alert('HIT!')
+            hits = hits + 1
+            if (hits == 3) {
+                isSunk = true
+                alert('You sank my battleship!')
+            }
+        } else {
+            alert('MISS')
+        }
+    }
+}
+let stats = 'You took ' + guesses + ' guesses to sink the battleship, ' + 'which means your shooting accuracy was ' + (3 / guesses)
+alert(stats)
+
