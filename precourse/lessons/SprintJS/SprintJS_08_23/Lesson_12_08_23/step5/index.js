81ea8b115586774111f4e19af462980711fa85f7
sergeistetsko <sergeistetsko2021@yandex.by>
2024-01-21 14:43:36 +0300

    step 5

--- precourse/lessons/SprintJS/SprintJS_08_23/Lesson_12_08_23/step5/index.js
+++ precourse/lessons/SprintJS/SprintJS_08_23/Lesson_12_08_23/step5/index.js
@@ -1,3 +1,11 @@
-document.addEventListener('mousemove', (event) => {
-    console.log(('Координаты мыши: X= ' + event.clientX + ', Y= ' + event.clientY))
+const input = document.getElementById('myInput')
+const button = document.getElementById('myButton')
+const container = document.getElementById('container')
+
+button.addEventListener('click', function () {
+    const text = input.value
+    const newElement = document.createElement('p')
+    newElement.textContent = text
+    container.appendChild(newElement)
+    input.value = ''
 })
