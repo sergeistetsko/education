2c7e44f78135b7869bb92d27fcb045dc6f8cfe0a
sergeistetsko <sergeistetsko2021@yandex.by>
2024-01-21 06:42:02 +0300

    step 2

---
+++ precourse/lessons/SprintJS/SprintJS_09_23/Lesson_02_09_23/step2/index.js
@@ -0,0 +1,5 @@
+let arr = ['сдавайтесь', 'сдавайтесь', 'Не сдавайтесь', 'не сдавайтесь', 'никогда, черт вас подери не сдавайтесь', 'при первой же трудности всегда сдавайтесь']
+
+let correctArray = arr.slice(2,5)
+let correctString = correctArray.join(', ')
+console.log(correctString)
