61bf25665a35c100cd03049388a43e229915b7ac
sergeistetsko <sergeistetsko2021@yandex.by>
2024-01-24 08:25:04 +0300

    step 4

---
+++ precourse/lessons/SprintJS/SprintJS_09_23/Lesson_09_09_23/step4/index.js
@@ -0,0 +1,6 @@
+const button2 = document.querySelector('#button2')
+const output = document.querySelector('#output')
+
+button2.addEventListener('click', () => {
+    output.textContent = 'Кнопка 2 нажата!'
+})
