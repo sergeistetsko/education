1c460cc8a0b079301695984ecfef79ffc7a18bd1
sergeistetsko <sergeistetsko2021@yandex.by>
2024-01-24 08:21:21 +0300

    step 3

---
+++ precourse/lessons/SprintJS/SprintJS_09_23/Lesson_09_09_23/step3/index.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    console.log('Кнопка нажата!');
+});
+
+const button2 = document.querySelector('#button2');
+button2.addEventListener('click', () => {
+    console.log('Кнопка 2 нажата!');
+});
