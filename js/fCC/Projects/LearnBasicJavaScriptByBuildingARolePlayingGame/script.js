5c5c0003b034638ade4e46fa51722ecc9c21538a
sergeistetsko <sergeistetsko2021@yandex.by>
2024-01-31 21:39:43 +0300

    add step 64 - 66

--- js/fCC/Projects/LearnBasicJavaScriptByBuildingARolePlayingGame/script.js
+++ js/fCC/Projects/LearnBasicJavaScriptByBuildingARolePlayingGame/script.js
@@ -50,12 +50,12 @@ const locations = [
 
 const monsterHealthText = document.querySelector('#monsterHealth')
 function update(location) {
-    button1.innerText = "Go to store";
-    button2.innerText = "Go to cave";
-    button3.innerText = "Fight dragon";
-    button1.onclick = goStore;
-    button2.onclick = goCave;
-    button3.onclick = fightDragon;
+    button1.innerText = location["button text"][0];
+    button2.innerText = location["button text"][1];
+    button3.innerText = location["button text"][2];
+    button1.onclick = location["button functions"][0];
+    button2.onclick = location["button functions"][1];
+    button3.onclick = location["button functions"][2];
     text.innerText = "You are in the town square. You see a sign that says \"Store\".";
 }
 function goTown() {
