0edb3c49612c95832d5ecd1bfc32f30e23919537
sergeistetsko <sergeistetsko2021@yandex.by>
2024-02-09 17:29:52 +0300

    add step 83 - 86

--- js/fCC/Projects/LearnBasicJavaScriptByBuildingARolePlayingGame/script.js
+++ js/fCC/Projects/LearnBasicJavaScriptByBuildingARolePlayingGame/script.js
@@ -101,6 +101,10 @@ function buyHealth() {
 function buyWeapon() {
     if (gold >= 30) {
         gold -= 30;
+        currentWeapon++;
+        goldText.innerText = gold;
+        let newWeapon = weapons
+        text.innerText = "You now have a new weapon.";
     }
 }
 
