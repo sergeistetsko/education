83baa0391552d77c7a62ffd723c176dbc103e8bf
sergeistetsko <sergeistetsko2021@yandex.by>
2024-02-14 17:22:33 +0300

    add step 101 - 104

--- js/fCC/Projects/LearnBasicJavaScriptByBuildingARolePlayingGame/script.js
+++ js/fCC/Projects/LearnBasicJavaScriptByBuildingARolePlayingGame/script.js
@@ -119,7 +119,10 @@ function buyWeapon() {
 }
 function sellWeapon() {
     if (inventory.length > 1) {
-
+        gold += 15;
+        goldText.innerText = gold;
+        let currentWeapon = inventory.shift();
+        text.innerText = "You sold a " + currentWeapon + ".";
     }
 }
 
