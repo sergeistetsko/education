5f31a872965ac0861728342f21bb7edc5dc181a3
sergeistetsko <sergeistetsko2021@yandex.by>
2024-02-17 17:42:07 +0300

    add step 2

--- js/JSWithZeroOld/projects/calculator/index.js
+++ js/JSWithZeroOld/projects/calculator/index.js
@@ -3,50 +3,44 @@ const buttonMinus = document.getElementById('button-minus')
 const buttonMultiply = document.getElementById('button-multiply')
 const buttonDivide = document.getElementById('button-divide')
 
-function onButtonPlusClick() {
-    const input1 = document.getElementById('number1')
-    const input2 = document.getElementById('number2')
-
-    const number1 = Number(input1.value)
-    const number2 = Number(input2.value)
+const input1 = document.getElementById('number1')
+const input2 = document.getElementById('number2')
+function getNumber1() {
+    return Number(input1.value)
+}
+function getNumber2() {
+    return Number(input2.value)
+}
+function makeOperation(operationCode) {
     
-    let summa = number1 + number2
-    alert(summa)
+    if (operationCode === '+') {
+        let result = getNumber1() + getNumber2()
+        window.alert(result)
+    } else if(operationCode === '-') {
+        let result = getNumber1() - getNumber2()
+        window.alert(result)
+    } else if(operationCode === '*') {
+        let result = getNumber1() * getNumber2()
+        window.alert(result)
+    } else if(operationCode === '/') {
+        let result = getNumber1() / getNumber2()
+        window.alert(result)
+    } else {
+        window.alert('operation is unknown')
+    }
 }
-
+function onButtonPlusClick() {
+    makeOperation('+')
+}
 function onButtonMinusClick() {
-    const input1 = document.getElementById('number1')
-    const input2 = document.getElementById('number2')
-
-    const number1 = Number(input1.value)
-    const number2 = Number(input2.value)
-
-    let minus = number1 - number2
-    alert(minus)
+    makeOperation('-')
 }
-
 function onButtonMultiplyClick() {
-    const input1 = document.getElementById('number1')
-    const input2 = document.getElementById('number2')
-
-    const number1 = Number(input1.value)
-    const number2 = Number(input2.value)
-
-    let multiply = number1 * number2
-    alert(multiply)
+    makeOperation('*')
 }
-
 function onButtonDivideClick() {
-    const input1 = document.getElementById('number1')
-    const input2 = document.getElementById('number2')
-
-    const number1 = Number(input1.value)
-    const number2 = Number(input2.value)
-
-    let divide = number1 / number2
-    alert(divide)
+    makeOperation('/')
 }
-
 buttonPlus.addEventListener('click', onButtonPlusClick)
 buttonMinus.addEventListener('click', onButtonMinusClick)
 buttonMultiply.addEventListener('click', onButtonMultiplyClick)
