416dbaa7da07f9dae884c39d4e980116852c2a4d
sergeistetsko <sergeistetsko2021@yandex.by>
2024-02-17 18:51:16 +0300

    add step 105 - 111

--- js/fCC/Projects/LearnBasicJavaScriptByBuildingARolePlayingGame/script.js
+++ js/fCC/Projects/LearnBasicJavaScriptByBuildingARolePlayingGame/script.js
@@ -35,6 +35,24 @@ const weapons = [
     }
 ];
 
+const monsters = [
+    {
+       name: "slime",
+       level: 2,
+       health: 15
+    },
+    {
+        name: "fanged beast",
+        level: 8,
+        health: 60
+    },
+    {
+        name: "dragon",
+        level: 20,
+        health: 300
+    }
+]
+
 const locations = [
     {
         name: "town square",
@@ -83,10 +101,6 @@ function goCave() {
     update(locations[2]);
 }
 
-function fightDragon() {
-    console.log("Fighting dragon.");
-}
-
 function buyHealth() {
     if (gold >= 10) {
         gold -= 10;
@@ -123,13 +137,34 @@ function sellWeapon() {
         goldText.innerText = gold;
         let currentWeapon = inventory.shift();
         text.innerText = "You sold a " + currentWeapon + ".";
+        text.innerText += " In your inventory you have: " + inventory;
+    } else {
+        text.innerText = "Don't sell your only weapon!";
     }
 }
 
 function fightSlime() {
-
+    fighting = 0
+    goFight()
 }
 
 function fightBeast() {
+    fighting = 1
+    goFight()
+}
+
+function fightDragon() {
+    fighting = 2
+    goFight()
+}
+
+function goFight() {
+
+}
+
+function attack() {
+
+}
+function dodge() {
 
 }
