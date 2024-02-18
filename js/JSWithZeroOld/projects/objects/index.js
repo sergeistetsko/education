c736f31387a80e6d72e702beb224c188e6291e9d
sergeistetsko <sergeistetsko2021@yandex.by>
2024-02-18 11:58:34 +0300

    add objects

---
+++ js/JSWithZeroOld/projects/objects/index.js
@@ -0,0 +1,12 @@
+const car = {
+    name: 'Reno StepWay',
+    isTurnOn: false,
+    speed: 0,
+    engine: {
+        v: 1.6,
+        horsesPower: 120
+    }
+}
+
+console.log(car.name)
+console.log(car.engine.v)
