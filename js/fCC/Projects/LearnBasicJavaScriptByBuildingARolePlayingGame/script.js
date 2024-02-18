6bcc07b5383fbb96aee66194485828de25a68c07
sergeistetsko <sergeistetsko2021@yandex.by>
2024-02-18 18:26:26 +0300

    add step 112 - 116

--- js/fCC/Projects/LearnBasicJavaScriptByBuildingARolePlayingGame/script.js
+++ js/fCC/Projects/LearnBasicJavaScriptByBuildingARolePlayingGame/script.js
@@ -71,6 +71,12 @@ const locations = [
         "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
         "button functions": [fightSlime, fightBeast, goTown],
         text: "You enter the cave. You see some monsters."
+    },
+    {
+        name: "fight",
+        "button text": ["Attack", "Dodge", "Run"],
+        "button functions": [attack, dodge, goTown],
+        text: "You are fighting a monster."
     }
 ];
 
@@ -159,7 +165,11 @@ function fightDragon() {
 }
 
 function goFight() {
-
+    update(locations[3])
+    monsterHealth = monsters[fighting].health;
+    monsterStats.style.display = 'block';
+    monsterName.innerText = monsters[fighting].name;
+    monsterHealthText.innerText = monsterHealth;
 }
 
 function attack() {
