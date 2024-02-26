d9339a5e0a4ed43e88bcf4ea87d7baf5c688af9a
sergeistetsko <sergeistetsko2021@yandex.by>
2024-02-26 20:20:53 +0300

    add step 131 - 134

--- js/fCC/Projects/LearnBasicJavaScriptByBuildingARolePlayingGame/script.js
+++ js/fCC/Projects/LearnBasicJavaScriptByBuildingARolePlayingGame/script.js
@@ -77,6 +77,12 @@ const locations = [
         "button text": ["Attack", "Dodge", "Run"],
         "button functions": [attack, dodge, goTown],
         text: "You are fighting a monster."
+    },
+    {
+        name: "kill monster",
+        "button text": ["Go to town square", "Go to town square", "Go to town square"],
+        "button functions": [goTown, goTown, goTown],
+        text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
     }
 ];
 
@@ -86,6 +92,7 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location) {
+    monsterStats.style.display = 'none';
     button1.innerText = location["button text"][0];
     button2.innerText = location["button text"][1];
     button3.innerText = location["button text"][2];
@@ -190,7 +197,7 @@ function dodge() {
 }
 
 function lose() {
-
+    update(locations[5])
 }
 
 function defeatMonster() {
