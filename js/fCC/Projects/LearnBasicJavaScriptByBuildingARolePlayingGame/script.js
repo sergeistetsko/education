9d568ddb9d4659adb8e5af21c5885c776fca4d81
sergeistetsko <sergeistetsko2021@yandex.by>
2024-03-04 19:13:17 +0300

    add step 152 - 161

--- js/fCC/Projects/LearnBasicJavaScriptByBuildingARolePlayingGame/script.js
+++ js/fCC/Projects/LearnBasicJavaScriptByBuildingARolePlayingGame/script.js
@@ -95,6 +95,12 @@ const locations = [
         "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
         "button functions": [restart, restart, restart],
         text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;"
+    },
+    {
+        name: "easter egg",
+        "button text": ["2", "8", "Go to town square?"],
+        "button functions": [pickTwo, pickEight, goTown],
+        text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!"
     }
 ];
 
@@ -211,6 +217,10 @@ function attack() {
             defeatMonster();
         }
     }
+    if (Math.random() <= .1 && inventory.length !== 1) {
+        text.innerText += " Your " + inventory.pop() + " breaks."
+        currentWeapon--;
+    }
 }
 
 function getMonsterAttackValue(level) {
@@ -220,7 +230,7 @@ function getMonsterAttackValue(level) {
 }
 
 function isMonsterHit() {
-    return Math.random() > .2;
+    return Math.random() > .2 || health < 20;
 }
 
 function dodge() {
@@ -248,6 +258,24 @@ function restart() {
 
     goTown();
 }
+
+function easterEgg() {
+    update(locations[7])
+}
+function pickTwo(guess) {
+    pick(2)
+}
+function pickEight(guess) {
+    pick(8)
+}
+
+function pick(guess) {
+    const numbers = [];
+    while (numbers.length < 10) {
+
+    }
+}
+
 function defeatMonster() {
     gold += Math.floor(monsters[fighting].level * 6.7);
     xp += monsters[fighting].level
