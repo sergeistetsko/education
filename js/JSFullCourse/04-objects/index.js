3c1b75eb55fc2e2c57644d65e8de1cc202d622f2
sergeistetsko <sergeistetsko2021@yandex.by>
2024-03-09 08:34:37 +0300

    add 04-objects part 2

--- js/JSFullCourse/04-objects/index.js
+++ js/JSFullCourse/04-objects/index.js
@@ -53,3 +53,85 @@ const countryPropertyName = 'country'
 myCity2[countryPropertyName] = 'USA'
 
 console.log(myCity2)
+
+// Урок 3 - Работа с вложенными объектами в JS--------------------------------------------------------------
+
+const myCity3 = {
+    city: 'New York',
+    info: {
+        isPopular: true,
+        country: 'USA'
+    }
+}
+
+console.log(myCity3.info.isPopular)
+
+delete myCity3.info['isPopular']
+
+console.log(myCity3.info.isPopular)
+
+// Урок 4 - Сокращённый формат записи свойств--------------------------------------------------------------
+
+// Использование переменных
+
+// Сокращённые свойства лучше использовать в начале объекта
+
+const name = 'Bogdan'
+const postsQty = 23
+
+const userProfile = {
+    name,
+    postsQty,
+    hasSignedAgreement: false
+}
+
+console.log(userProfile)
+
+// Урок 5 - Глобальные объекты------------------------------------------------------------------------------
+
+// Глобальный объект - window - в веб-браузере - объект с огромным количеством свойств и методов
+
+// У него есть методы и свойства, которые доступны глобально
+
+// Урок 6 - Методы объекта -----------------------------------------------------------------------------------
+
+// Метод объекта - свойство объекта, значение которого функция
+
+const myCity4 = {
+    city: 'New York',
+    cityGreeting: function () {
+        console.log('Greetings!!!')
+    }
+}
+
+myCity4.cityGreeting()
+
+// Также можно создать метод без ключевого слова function и :
+
+const myCity5 = {
+    city: 'New York',
+    cityGreeting() {
+        console.log('Greetings!!!')
+    }
+}
+
+myCity5.cityGreeting() // Для того чтобы вызвать метод, необходимо указать круглые скобки и через точечную нотацию достучаться к объекту
+
+// Урок 7 - JSON -----------------------------------------------------------------------------------
+
+// Все свойства находятся в двойных кавычках и используется для обмена данными в интернете
+
+// Как происходит передача данных, в виде строки ?
+
+// Конвертация происходит через JSON.parse() получив .json можно сконвертировать его в объект
+// Наоборот JSON.stringify() переводит js-объект в json
+
+const post = {
+    title: 'My post',
+    likesQty: 5
+}
+const postStringified = JSON.stringify(post)
+
+JSON.parse(postStringified) // распарсили JSON объект в JS-объект
+
+
