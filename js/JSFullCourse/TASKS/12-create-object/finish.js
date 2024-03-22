a9aad8988ddb818cdf49fbac52de7eb85d57e01c
sergeistetsko <sergeistetsko2021@yandex.by>
2024-03-22 20:28:03 +0300

    add task 12 solution

--- js/JSFullCourse/TASKS/12-create-object/finish.js
+++ js/JSFullCourse/TASKS/12-create-object/finish.js
@@ -8,3 +8,11 @@
  * 2. Выведите в консоль строку
  * "My name is <name> <surname> and my favorite number is <favoriteNumber>"
  */
+
+const myInfo = {
+    name: 'Sergey',
+    surname: 'X',
+    favoriteNumber: 3
+}
+
+console.log(`My name is ${myInfo.name} ${myInfo.surname} and my favorite number is ${myInfo.favoriteNumber}`)
