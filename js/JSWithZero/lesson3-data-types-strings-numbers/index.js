1f94fc431cec251b64e9bd1851349d51ac392fcc
sergeistetsko <sergeistetsko2021@yandex.by>
2024-03-23 11:54:44 +0300

    add lesson3-data-types-strings-numbers

---
+++ js/JSWithZero/lesson3-data-types-strings-numbers/index.js
@@ -0,0 +1,24 @@
+/*
+function greeting() {
+    alert('Привет, мир!!!')
+}
+setTimeout(greeting, 5000)*/
+
+const friendName = 'Alex'
+// friendName = 'Bob'
+
+let friendAge  = 28
+
+friendAge = 29
+
+// alert('Привет, ' + friendName)
+
+const price = 100
+const discount = 10
+const total = price - (price * discount / 100)
+
+console.log(total)
+
+console.log(total > 90)
+console.log(total === 100)
+console.log(total < 100)
