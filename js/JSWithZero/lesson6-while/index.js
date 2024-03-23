b40f290b26498e9f070d56ea8bbdbc2da9375539
sergeistetsko <sergeistetsko2021@yandex.by>
2024-03-23 13:13:24 +0300

    add lesson6-while

---
+++ js/JSWithZero/lesson6-while/index.js
@@ -0,0 +1,49 @@
+/*
+function greeting() {
+    alert('Привет, мир!!!')
+}
+setTimeout(greeting, 5000)*/
+
+// const friendName = 'Alex'
+// friendName = 'Bob'
+
+// let friendAge  = 28
+
+// friendAge = 29
+
+// alert('Привет, ' + friendName)
+
+/*const price = 100
+const discount = 12
+const total = price - (price * discount / 100)
+
+if (total >= 90) {
+    console.log('Не берём')
+} else if (total < 80) {
+    console.log('Дайте два!!!')
+} else {
+    console.log('Берём один')
+}*/
+
+// console.log(total)
+//
+// console.log(total > 90)
+// console.log(total === 100)
+// console.log(total < 100)
+
+// В данном уроке было просто пояснение про var и строгое сравнение
+
+let sum = 1000
+const percent = 15
+const result = 2000
+
+let count = 0
+
+while (sum < result) {
+    sum = sum + sum * (percent / 100)
+    console.log(sum)
+    count++
+}
+
+console.log(sum, count)
+
