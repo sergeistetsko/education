97d2dce8eb97ddb01e5b980101a624f7735913e3
sergeistetsko <sergeistetsko2021@yandex.by>
2024-03-23 17:52:19 +0300

    add lesson17-arrays

---
+++ js/JSWithZero/lesson17-arrays/index.js
@@ -0,0 +1,16 @@
+const scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54]
+
+console.log(scores[0])
+console.log(scores[2])
+
+const pets = ['dog', 'cat', 'rat']
+
+console.log(pets[0])
+
+pets[2] = 'parrot'
+console.log(pets)
+
+console.log(scores)
+
+
+
