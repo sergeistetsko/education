2b3a623e77d78fe78694b017b0c845a9ee33c6c6
sergeistetsko <sergeistetsko2021@yandex.by>
2024-03-23 16:56:10 +0300

    add lesson13-functions-acquaintance

---
+++ js/JSWithZero/lesson13-functions-acquaintance/index.js
@@ -0,0 +1,12 @@
+function bark(name, weight) {
+    if (weight > 20) {
+        console.log(name + ' says WOOF WOOF')
+    } else {
+        console.log(name + ' says woof woof')
+    }
+}
+
+bark('Rover', 53)
+bark('Spot', 13)
+bark('Spike', 53)
+bark('Lady', 17)
