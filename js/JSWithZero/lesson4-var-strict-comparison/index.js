ec25e28db91a2f45b10f0c30854b6e7682b3b5c6
sergeistetsko <sergeistetsko2021@yandex.by>
2024-03-23 12:00:05 +0300

    add lesson4-var-strict-comparison

---
+++ js/JSWithZero/lesson4-var-strict-comparison/index.js
@@ -0,0 +1,26 @@
+/*
+function greeting() {
+    alert('Привет, мир!!!')
+}
+setTimeout(greeting, 5000)*/
+
+const friendName = 'Alex'
+// friendName = 'Bob'
+
+let friendAge  = 28
+
+friendAge = 29
+
+// alert('Привет, ' + friendName)
+
+const price = 100
+const discount = 10
+const total = price - (price * discount / 100)
+
+console.log(total)
+
+console.log(total > 90)
+console.log(total === 100)
+console.log(total < 100)
+
+// В данном уроке было просто пояснение про var и строгое сравнение
